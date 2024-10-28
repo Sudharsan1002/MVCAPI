8d34ca53fc392562e0fc73fba04cbf23211dcefc
Sudharsan1002 <sudharsanselva2003@gmail.com>
2024-10-28 14:43:21 +0530

    first commit

---
+++ controllers/Recipes.Controller.js
@@ -0,0 +1,108 @@
+const RecipesModel = require("../model/Recipesmodel");
+
+const RecipesRouter = require("express").Router();
+
+// FETCHES ALL RECIPES
+RecipesRouter.get("/", async function (request, response) {
+  try {
+    const results = await RecipesModel.find();
+    return response.status(200).json({
+      message: "Recipes fetched successfully",
+      data: results,
+    });
+  } catch (error) {
+    return response.status(500).json({
+      message: "something went wrong",
+      error: error.message,
+    });
+  }
+});
+
+// FETCHES ORDER MATCHES GIVEN RECIPEID
+RecipesRouter.get("/:recipeId", async function (request, response) {
+  const recipeId = request.params.recipeId.trim();
+  try {
+    const matchedRecipe = await RecipesModel.findById(recipeId);
+    if (!matchedRecipe) {
+      return response.status(404).json({
+        message: "No Recipe found",
+      });
+    }
+    return response.status(200).json({
+      message: "Recipe fetched successfully",
+      matchedRecipe,
+    });
+  } catch (error) {
+    return response.status(500).json({
+      message: "Error fetching recipe",
+      error: error.message,
+    });
+  }
+});
+
+// CREATE A RECIPE
+RecipesRouter.post("/createRecipe", async function (request, response) {
+  try {
+    const result = await RecipesModel.create(request.body);
+
+    return response.status(200).json({
+      message: "Recipe created successfully",
+      data: result,
+    });
+  } catch (error) {
+    return response.status(400).json({
+      message: "Data is missing",
+      error: error.message,
+    });
+  }
+});
+
+// FIND AND UPDATE RECIPE
+
+RecipesRouter.put("/:recipeId", async function (request, response) {
+  const recipeId = request.params.recipeId.trim();
+  try {
+    const updatedRecipe = await RecipesModel.findByIdAndUpdate(
+      recipeId,
+      request.body,
+      { new: true }
+    );
+    if (!updatedRecipe) {
+      return response.status(404).json({
+        message: "Recipe not found",
+      });
+    }
+    return response.status(200).json({
+      message: "Recipe updated successfully",
+      updatedRecipe,
+    });
+  } catch (error) {
+    return response.status(500).json({
+      message: "Update failed",
+      error: error.message,
+    });
+  }
+});
+
+// DELETE RECIPE BY ID
+
+RecipesRouter.delete("/:recipeId", async function (request, response) {
+  const recipeId = request.params.recipeId;
+  try {
+    const deletedRecipe = await RecipesModel.findByIdAndDelete(recipeId);
+    if (!deletedRecipe) {
+      return response.status(404).json({ message: "Recipe not found" });
+    }
+    return response.status(200).json({
+      message: "Successfully deleted",
+      deletedRecipe,
+    });
+  } catch (error) {
+    return response.status(500).json({
+      message: "Deletion failed",
+      error: error.message,
+    });
+  }
+});
+
+module.exports = RecipesRouter;
---
+++ db.js
@@ -0,0 +1,17 @@
+const mongoose = require("mongoose");
+
+const MONGODBURI = "mongodb://localhost:27017/recipes1";
+
+// FUNCTION TO CREATE DATABASE CONNECTIVITY
+async function createDbConnection() {
+  try {
+    const response = await mongoose.connect(MONGODBURI);
+    console.log("connection established");
+  } catch (error) {
+    console.log(error.message);
+  }
+}
+
+module.exports = {
+  createDbConnection,
+};
